1eaa5e059e258e4a12e181d83a56a35d0661945c
Jim Peterson <omesadev@outlook.com>
2020-09-29 13:40:52 -0400

    Remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -17,14 +17,6 @@ test('should render Header correctly', () => {
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water Bill', amount: 4500 }));
-store.dispatch(addExpense({ description: 'Gas Bill', createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
