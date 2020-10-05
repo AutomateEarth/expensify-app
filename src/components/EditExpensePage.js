71dfc93951dd77d9b3cf5a2884e9195eaccdc90e
Jim Peterson <omesadev@outlook.com>
2020-10-05 08:29:04 -0400

    Minor change

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -31,7 +31,7 @@ const mapStateToProps = (state, props) => ({
 
 const mapDispatchToProps = (dispatch, props) => ({
     editExpense: (id, expense) => dispatch(editExpense(id, expense)),
-    removeExpense: () => dispatch(removeExpense(data))
+    removeExpense: () => dispatch(removeExpense(props.data))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(EditExpensePage);
